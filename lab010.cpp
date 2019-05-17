c2b9d8c685b7e37d777455a0e97a89f3637a4a4b
nebula912 <nebula912@outlook.com>
2019-05-17 12:47:17 +0800

    add a new file

---
+++ lab010.cpp
@@ -0,0 +1,9 @@
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/imgcodecs.hpp>
+using namespace cv;
+using namespace std;
+//This demo processes the image degraded by motion.
+//i.e. motion deblur filter. 
+//using Wiener filter.
+
